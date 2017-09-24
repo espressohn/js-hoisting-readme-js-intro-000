6301406a28fb57e9dd1f954215478b7d3e7786e5
espressohn <espressohn@gmail.com>
2017-09-24 11:35:50 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -5,7 +5,7 @@ function callMe() {
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  
+
 }
 
 
@@ -28,5 +28,5 @@ function sayMyName() {
   }
   sayMy();
 
-  
+
 }
